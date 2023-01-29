d843d7e00276ddfb233263931eac17b02a1a3497
EmaleeSoto <emaleesoto@gmail.com>
2023-01-29 01:58:50 -0500

    Added customer routes

--- back-end/controllers/customerController.js
+++ back-end/controllers/customerController.js
@@ -8,8 +8,9 @@ const {
   getOneCustomer,
   getOneCustomerByFirebase,
   createCustomer,
+  updateCustomer,
+  deleteCustomer,
 } = require("../queries/customer");
-const { all } = require("./productsController");
 
 customer.get("/", async (req, res) => {
   try {
@@ -25,7 +26,7 @@ customer.get("/:id", async (req, res) => {
     const oneCustomer = await getOneCustomer(req.params.id);
     res.status(200).json({ success: true, payload: oneCustomer });
   } catch (error) {
-    return error;
+    res.status(400).json({ success: false, error: "Could not find this user" });
   }
 });
 
@@ -36,7 +37,7 @@ customer.get("/firebase/:id", async (req, res) => {
     );
     res.status(200).json({ success: true, payload: oneCustomerByFirebaseId });
   } catch (error) {
-    return error;
+    res.status(400).json({ success: false, error: "Could not find this user" });
   }
 });
 
@@ -46,6 +47,24 @@ customer.post("/", async (req, res) => {
     res.status(200).json({ success: true, payload: newCustomer });
   } catch (error) {
     res.status(400).json({ success: false, error: error });
+  }
+});
+
+customer.put("/:id", async (req, res) => {
+  try {
+    const updatedCustomer = await updateCustomer(req.params.id, req.body);
+    res.status(200).json({ success: true, payload: updatedCustomer });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+customer.delete("/:id", async (req, res) => {
+  const deletedCustomer = await deleteCustomer(req.params.id);
+  if (deletedCustomer) {
+    res.status(200).json({ success: true, payload: deletedCustomer });
+  } else {
+    res.status(404).json({ success: false, payload: "User not found" });
   }
 });
 
--- back-end/queries/customer.js
+++ back-end/queries/customer.js
@@ -58,9 +58,55 @@ const createCustomer = async ({
   }
 };
 
+const updateCustomer = async (
+  id,
+  {
+    name,
+    phone_number,
+    email,
+    street_address,
+    postal_code,
+    city,
+    state,
+    email_list,
+    firebase_id,
+  }
+) => {
+  try {
+    return await db.one(
+      "UPDATE customer SET name=$1, phone_number=$2, email=$3, street_address=$4, postal_code=$5, city=$6, state=$7, email_list=$8, firebase_id=$9 WHERE id=$10 RETURNING *",
+      [
+        name,
+        phone_number,
+        email,
+        street_address,
+        postal_code,
+        city,
+        state,
+        email_list,
+        firebase_id,
+        id,
+      ]
+    );
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteCustomer = async (id) => {
+  try {
+    return await db.one("DELETE FROM customer WHERE id=$1 RETURNING *", id);
+  } catch (error) {
+    console.log(error.message || error);
+    return error;
+  }
+};
+
 module.exports = {
   getAllCustomers,
   getOneCustomer,
   getOneCustomerByFirebase,
   createCustomer,
+  updateCustomer,
+  deleteCustomer,
 };
